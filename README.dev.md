fe2b87794960f712aad49b59cefaf6562251f12a
Jyotendra <jyotendrasharma92@gmail.com>
2021-06-02 13:24:26 +0530

    added cognito support

---
+++ README.dev.md
@@ -0,0 +1,11 @@
+## Env Variables and their implications
+
+Following environment variables needs to be defined in ".env.development" and ".env.production" as per the use-case:
+
+REACT_APP_API_URL=/api
+REACT_APP_COGNITO_REGION=ap-south-1
+REACT_APP_USER_POOL_ID=ap-south-1_tmLHLojwT
+REACT_APP_USER_POOL_WEB_CLIENT_ID=7ba9k8pgjag1rjh3kdlijser1h
+REACT_APP_COGNITO_DOMAIN=sakura.auth.ap-south-1.amazoncognito.com
+REACT_APP_SIGNIN_REDIRECT=https://sakurastorefront.netlify.app/
+REACT_APP_SIGNOUT_REDIRECT=https://sakurastorefront.netlify.app/
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,71 @@
 import AppRoutes from "./routes";
 import "./App.scss";
 import AppLayout from "./pages/appLayout";
-import {API_URL} from "./shared/consts";
+import Amplify, { Auth, Hub } from "aws-amplify";
+import React from "react";
 
-console.log(API_URL);
+Amplify.configure({
+  Auth: {
+    region: process.env["REACT_APP_COGNITO_REGION"],
+    userPoolId: process.env["REACT_APP_USER_POOL_ID"],
+    userPoolWebClientId: process.env["REACT_APP_USER_POOL_WEB_CLIENT_ID"],
+    oauth: {
+      domain: process.env["REACT_APP_COGNITO_DOMAIN"],
+      scope: [
+        "phone",
+        "email",
+        "profile",
+        "openid",
+        "aws.cognito.signin.user.admin",
+      ],
+      redirectSignIn: process.env["REACT_APP_SIGNIN_REDIRECT"],
+      redirectSignOut: process.env["REACT_APP_SIGNOUT_REDIRECT"],
+      responseType: "code", // or 'token', note that REFRESH token will only be generated when the responseType is code
+    },
+  },
+});
 
-function App() {
-  return (
-    <div>
+class App extends React.Component<any, any> {
+  state = { user: null, customState: null };
+
+  constructor(props: any) {
+    super(props);
+  }
+
+  componentDidMount() {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          this.setState({ user: data });
+          break;
+        case "signOut":
+          this.setState({ user: null });
+          break;
+        case "customOAuthState":
+          this.setState({ customState: data });
+      }
+    });
+
+    Auth.currentAuthenticatedUser()
+      .then((user) => this.setState({ user }))
+      .catch(() => console.log("Not signed in"));
+  }
+
+  render() {
+    return (
       <div>
-        <AppLayout>
-          <AppRoutes />
-        </AppLayout>
+        <div className="App">
+          <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+          <button onClick={() => Auth.signOut()}>Sign Out</button>
+        </div>
+        <div>
+          <AppLayout>
+            <AppRoutes />
+          </AppLayout>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
